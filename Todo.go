6215e17e455a08032f078e3561f7f8b5511bcc45
David Tapson <dave.tapson@gmail.com>
2017-08-22 21:54:23 +0200

    Finished off as per website tutorial

--- Todo.go
+++ Todo.go
@@ -1,8 +1,11 @@
 package main
 
-import "time"
+import (
+	"time"
+)
 
 type Todo struct {
+	Id int `json: "id"`
 	Name      string `json:"name"`
 	Completed bool `json:"completed"`
 	Due       time.Time `json:due`
